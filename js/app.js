9e3dcb4390e9c83a75d3d74808bccd182da77765
c0d3sh0rt4g3 <eriking2004@gmail.com>
2023-10-26 13:18:50 +0200

    Done

--- js/app.js
+++ js/app.js
@@ -1,16 +1,42 @@
 const submitButton = document.querySelector(".button-primary")
-const messageList = document.querySelector(".lista-mensajes")
-const storedMessages = []
+const deleteButton = document.querySelector("#delete-button")
+const messageListDiv = document.querySelector("#lista-mensajes")
+const storedMessages = JSON.parse(localStorage.getItem("messages"))
+const messageList = document.createElement("ul")
 submitButton.addEventListener("click", () => {
-    const message = document.querySelector("#mensaje").value
-    storedMessages.push(message)
-    addMessageToPage(storedMessages)
+  event.preventDefault()
+  const message = document.querySelector("#mensaje").value
+  storedMessages.push(message)
+  localStorage.setItem("messages", JSON.stringify(storedMessages))
+  addMessageToPage(message)
 })
-const addMessageToPage = (storedMessages) =>{
+
+document.addEventListener("DOMContentLoaded", () =>{
+    addAllMessagesToPage(storedMessages)
+})
+
+deleteButton.addEventListener("click", () =>{
+    deleteMessages()
+})
+
+const addMessageToPage = (message) => {
+    const messageListElement = document.createElement("li")
+    messageListElement.classList.add("message")
+    messageListElement.textContent = message
+    messageList.appendChild(messageListElement)
+    messageListDiv.appendChild(messageList)
+}
+
+const addAllMessagesToPage = (storedMessages) =>{
     storedMessages.forEach(message =>{
-        const messageDiv = document.createElement("div")
-        messageDiv.classList.add("message")
-        messageDiv.textContent = message
-        messageList.appendChild(messageDiv)
+        addMessageToPage(message)
     })
+}
+
+const deleteMessages = () =>{
+    messageListDiv.firstChild.remove()
+    while (storedMessages.length > 0) {
+        storedMessages.pop();
+    }
+    localStorage.setItem("messages", JSON.stringify([]))
 }
